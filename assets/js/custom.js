dab7c783d6fdccee7ab1283037edd60810a7ea9a
mdshukur <90003137+mdshukur@users.noreply.github.com>
2024-02-20 00:05:00 +0600

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -64,3 +64,24 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+// testimonials
+$(function() {
+  
+  
+  $('.slide-one-item').owlCarousel({
+      center: false,
+      autoplayHoverPause: true,
+      items: 1,
+      loop: true,
+      stagePadding: 0,
+      margin: 0,
+      smartSpeed: 1500,
+      autoplay: true,
+      pauseOnHover: false,
+      dots: true,
+      nav: true,
+      navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+  });
+  
+  
+  })
